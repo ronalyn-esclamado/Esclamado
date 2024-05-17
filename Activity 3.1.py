50e92100e021fd85554debe29dfdb3916dafdd9e
ronalyn-esclamado <ollenesclamado@gmail.com>
2024-05-17 16:55:24 +0800

    Add files via upload

---
+++ Activity 3.1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 15:21:47 2024
+
+@author: Esclamado
+"""
+import numpy as np 
+import matplotlib.pyplot as plt
+
+prior_probs = np.array([[0.33,0.3],[0.2,0.17]])
+
+plt.imshow(prior_probs, cmap= 'gray')
+plt.colarbar()
+
+for i in range (2):
+    for j in range (2):
+        plt.annotate(prior_probs[i,j], (j,i), color="red", fontsize=20, fontweight='bold', ha='center', va='center')
+        
+plt.title('Prior Probabilities', fontsize=20)
+        
